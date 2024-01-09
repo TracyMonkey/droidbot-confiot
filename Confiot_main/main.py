00a04199925c360c28322f5870a512cfe63408e4
just1cep4rtn3r@gmail.com <just1cep4rtn3r@gmail.com>
2024-01-09 01:51:29 -0800

    update

--- Confiot_main/main.py
+++ Confiot_main/main.py
@@ -69,32 +69,32 @@ def Action(host, guest):
 
 
 if __name__ == "__main__":
-    os.environ["https_proxy"] = "http://192.168.72.1:1083"
+    # os.environ["https_proxy"] = "http://192.168.72.1:1083"
 
-    s = settings("14131FDF600073", "/root/documents/Output/Huawei_AI_Life/Huawei.apk",
-                 "/root/documents/Output/Huawei_AI_Life/host/result")
-    # HostActor = HostInitialization()
-    HostAction(None, "2. Remove an alarm", "015ba3ec79e0b0f55a19ce31bbc72b503e56184e14e0cef46ad942d8d357f489")
+    # s = settings("14131FDF600073", "/root/documents/Output/Huawei_AI_Life/Huawei.apk",
+    #              "/root/documents/Output/Huawei_AI_Life/host/result")
+    # # HostActor = HostInitialization()
+    # HostAction(None, "2. Remove an alarm", "015ba3ec79e0b0f55a19ce31bbc72b503e56184e14e0cef46ad942d8d357f489")
 
-    # parser = OptionParser()
-    # parser.add_option("-a", "--app-path", dest="app_path", help="The apk path of the target application")
-    # parser.add_option("-d", "--device", dest="device", help="The device serial")
-    # parser.add_option("-D", "--droidbot-output", dest="droid_output", help="The output path of droidbot")
-    # parser.add_option("-H", "--host", dest="host", action="store_true", default=False, help="Host")
-    # parser.add_option("-G", "--guest", dest="guest", action="store_true", default=False, help="Guest")
-    # parser.add_option("-b", "--director", dest="director", action="store_true", default=False, help="Director Mode")
-    # parser.add_option("-c", "--configuration", dest="config", help="Configuration File")
+    parser = OptionParser()
+    parser.add_option("-a", "--app-path", dest="app_path", help="The apk path of the target application")
+    parser.add_option("-d", "--device", dest="device", help="The device serial")
+    parser.add_option("-D", "--droidbot-output", dest="droid_output", help="The output path of droidbot")
+    parser.add_option("-H", "--host", dest="host", action="store_true", default=False, help="Host")
+    parser.add_option("-G", "--guest", dest="guest", action="store_true", default=False, help="Guest")
+    parser.add_option("-b", "--director", dest="director", action="store_true", default=False, help="Director Mode")
+    parser.add_option("-c", "--configuration", dest="config", help="Configuration File")
 
-    # (options, args) = parser.parse_args()
+    (options, args) = parser.parse_args()
 
-    # s = settings(options.device, options.app_path, options.droid_output)
+    s = settings(options.device, options.app_path, options.droid_output)
 
-    # HostActor = None
-    # GuestActor = None
+    HostActor = None
+    GuestActor = None
 
-    # if (options.host):
-    #     # HostActor = HostInitialization()
-    #     HostAction(None, "1. Set up an alarm on Huawei AI Speaker",
-    #                "015ba3ec79e0b0f55a19ce31bbc72b503e56184e14e0cef46ad942d8d357f489")
-    # elif (options.guest):
-    #     GuestActor = GuestInitialization()
+    if (options.host):
+        # HostActor = HostInitialization()
+        HostAction(None, "1. Set up an alarm on Huawei AI Speaker",
+                   "015ba3ec79e0b0f55a19ce31bbc72b503e56184e14e0cef46ad942d8d357f489")
+    elif (options.guest):
+        GuestActor = GuestInitialization()
